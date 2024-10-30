46d4ac2423c8ca1f73ea72a1d449647e878d15bd
DRagssss <121556682+DRagssss@users.noreply.github.com>
2024-10-30 16:31:59 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import interactions
+import glob
+import os
+
+bot = interactions.Client(
+    intents=interactions.Intents.ALL, token="",
+  # debug_scope=0,
+)  
+interactions.prefixed_commands.setup(bot, default_prefix=".")
+
+
+@interactions.listen()
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+
+for extension in [
+    os.path.splitext(filename)[0].replace(os.path.sep, ".")
+    for filename in glob.glob(os.path.join("Extensions", "**", "*.py"), recursive=True)
+]:
+    bot.load_extension(extension)
+
+bot.start()
